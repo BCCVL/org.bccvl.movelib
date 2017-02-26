0f4ac103736498b11e1a665b15b6b4db0f15c73a
Gerhard Weis <g.weis@griffith.edu.au>
2017-02-27 08:45:08 +1000

    use standard init.py

--- src/org/__init__.py
+++ src/org/__init__.py
@@ -1,8 +1,8 @@
 # See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-# try:
-#     __import__('pkg_resources').declare_namespace(__name__)
-# except ImportError:
-#     from pkgutil import extend_path
-#     __path__ = extend_path(__path__, __name__)
-
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except (ImportError, AttributeError):
+    # AttributeError may be caused due to recursive import of pkg_resources
+    # let's fall back to legacy namespace packages in that case
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
